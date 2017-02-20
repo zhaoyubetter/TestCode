5ec278164a4d4332567869d0b8688cb7be0e889b
zhaoyu57 <zhaoyu1@jd.com>
2017-02-20 11:03:09 +0800

    事件分发的复习

---
+++ improve/src/main/java/test/better/com/leak/view/MotionEventsActivity.java
@@ -0,0 +1,19 @@
+package test.better.com.leak.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+/**
+ * Created by zhaoyu1 on 2017/2/17.
+ */
+
+public class MotionEventsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
