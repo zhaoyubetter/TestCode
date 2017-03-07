53e59e447e89c85433c6c109fa382a12b789ad7f
zhaoyu57 <zhaoyu1@jd.com>
2017-03-07 14:17:52 +0800

    修正 小 bug

--- basenet/src/main/java/better/basenet/volley/VolleyRequest.java
+++ basenet/src/main/java/better/basenet/volley/VolleyRequest.java
@@ -26,104 +26,102 @@ import better.basenet.base.request.AbsRequest;
  */
 public class VolleyRequest extends AbsRequest {
 
-	private static RequestQueue requestQueue;
-	private Request mRequest;
+    private static RequestQueue requestQueue;
+    private Request mRequest;
 
-	private VolleyRequest(Builder builder) {
-		super(builder);
-	}
+    private VolleyRequest(Builder builder) {
+        super(builder);
+        if (requestQueue == null) {
+            synchronized (VolleyRequest.class) {
+                if (requestQueue == null) {
+                    requestQueue = Volley.newRequestQueue(builder.mCtx);
+                }
+            }
+        }
+    }
 
-	@Override
-	protected void get() {
-		realRequest(Request.Method.GET);
-	}
+    @Override
+    protected void get() {
+        realRequest(Request.Method.GET);
+    }
 
-	@Override
-	protected void post() {
-		realRequest(Request.Method.POST);
-	}
+    @Override
+    protected void post() {
+        realRequest(Request.Method.POST);
+    }
 
-	private void realRequest(final int reqType) {
-		int tReqType = Request.Method.GET;
-		String tUrl = mUrl;
-		switch (tReqType) {
-			case Request.Method.GET:
-				tReqType = Request.Method.GET;
-				tUrl = generateUrl(mUrl, mParams);
-				break;
-			case Request.Method.POST:
-				tReqType = Request.Method.POST;
-				break;
-		}
+    private void realRequest(final int reqType) {
+        int tReqType = Request.Method.GET;
+        String tUrl = mUrl;
+        switch (tReqType) {
+            case Request.Method.GET:
+                tReqType = Request.Method.GET;
+                tUrl = generateUrl(mUrl, mParams);
+                break;
+            case Request.Method.POST:
+                tReqType = Request.Method.POST;
+                break;
+        }
 
-		mRequest = new StringRequest(tReqType, tUrl, new Response.Listener<String>() {
-			@Override
-			public void onResponse(String response) {
-				mCallBack.onSuccess(response);
-			}
-		}, new Response.ErrorListener() {
-			@Override
-			public void onErrorResponse(VolleyError error) {
-				mCallBack.onFailure(error);
-			}
-		}) {
-			@Override
-			public Map<String, String> getHeaders() throws AuthFailureError {
-				Map<String, String> superHeader = super.getHeaders();
-				if (mHeader != null && mHeader.size() > 0) {
-					superHeader = mHeader;
-				}
-				return superHeader;
-			}
+        mRequest = new StringRequest(tReqType, tUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mCallBack.onSuccess(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mCallBack.onFailure(error);
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> superHeader = super.getHeaders();
+                if (mHeader != null && mHeader.size() > 0) {
+                    superHeader = mHeader;
+                }
+                return superHeader;
+            }
 
-			// 设置Body参数
-			@Override
-			protected Map<String, String> getParams() throws AuthFailureError {
-				Map<String, String> tParams = super.getParams();
-				if (mParams != null && mParams.size() > 0 && reqType == Request.Method.POST) {
-					tParams = mParams;
-				}
-				return tParams;
-			}
-		};
+            // 设置Body参数
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> tParams = super.getParams();
+                if (mParams != null && mParams.size() > 0 && reqType == Request.Method.POST) {
+                    tParams = mParams;
+                }
+                return tParams;
+            }
+        };
 
-		// 设置此次请求超时时间
-		if (mTimeOut > 1000) {
-			mRequest.setRetryPolicy(new DefaultRetryPolicy((int) mTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-		}
-		mRequest.setTag(mTag);
-		requestQueue.add(mRequest);
-	}
+        // 设置此次请求超时时间
+        if (mTimeOut > 1000) {
+            mRequest.setRetryPolicy(new DefaultRetryPolicy((int) mTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        }
+        mRequest.setTag(mTag);
+        requestQueue.add(mRequest);
+    }
 
-	@Override
-	public void cancel() {
-		if (mRequest != null) {
-			mRequest.cancel();
-		} else if (mTag != null) {
-			requestQueue.cancelAll(mTag);
-		}
-	}
+    @Override
+    public void cancel() {
+        if (mRequest != null) {
+            mRequest.cancel();
+        } else if (mTag != null) {
+            requestQueue.cancelAll(mTag);
+        }
+    }
 
-	public static class Builder extends AbsRequest.Builder {
+    public static class Builder extends AbsRequest.Builder {
 
-		private Context mCtx;
-		private VolleyRequest sRequest;
+        private Context mCtx;
 
-		public Builder(Context ctx) {
-			this.mCtx = ctx;
-		}
+        public Builder(Context ctx) {
+            this.mCtx = ctx;
+        }
 
-		@Override
-		public AbsRequest build() {
-			if (sRequest == null) {
-				synchronized (VolleyRequest.class) {
-					if (sRequest == null) {
-						sRequest = new VolleyRequest(this);
-						requestQueue = Volley.newRequestQueue(mCtx);
-					}
-				}
-			}
-			return sRequest;
-		}
-	}
+        @Override
+        public AbsRequest build() {
+            return new VolleyRequest(this);
+        }
+    }
 }
