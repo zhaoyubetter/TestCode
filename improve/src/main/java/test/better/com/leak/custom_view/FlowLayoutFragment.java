3744f15ce99bc198e91e8e8b71e1251b8b68ccf4
zhaoyubetter@126.com <zhaoyubetter@126.com>
2017-03-19 00:02:29 +0800

    流布局例子

--- improve/src/main/java/test/better/com/leak/custom_view/FlowLayoutFragment.java
+++ improve/src/main/java/test/better/com/leak/custom_view/FlowLayoutFragment.java
@@ -1,12 +1,16 @@
 package test.better.com.leak.custom_view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.Random;
 
 import test.better.com.leak.R;
 import ui.better.FlowLayout;
@@ -18,22 +22,67 @@ import ui.better.FlowLayout;
 public class FlowLayoutFragment extends Fragment {
 
 	private FlowLayout flowLayout;
+	private SeekLayout seekLayout_ver;
+	private SeekLayout seekLayout_hor;
+	private RadioLayout radioGravity;
+
+	private View add;
+	private View del;
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_flow_layout, container, false);
 		flowLayout = (FlowLayout) view.findViewById(R.id.flowLayout);
+		seekLayout_ver = (SeekLayout) view.findViewById(R.id.seekLayout_ver);
+		seekLayout_hor = (SeekLayout) view.findViewById(R.id.seekLayout_hor);
+		radioGravity = (RadioLayout) view.findViewById(R.id.radioGravity);
 
-		flowLayout.postDelayed(new Runnable() {
+		add = view.findViewById(R.id.add);
+		del = view.findViewById(R.id.del);
+
+		seekLayout_ver.setOnSeekProgressChangeListener(new SeekLayout.OnSeekProgressChangeListener() {
 			@Override
-			public void run() {
-				TextView tv = new TextView(getContext());
-				tv.setLayoutParams(new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-				tv.setText("大招");
+			public void onProgressChanged(SeekBar seekBar, int progress) {
+				flowLayout.setVerticalPadding(progress);
+			}
+		});
+
+		seekLayout_hor.setOnSeekProgressChangeListener(new SeekLayout.OnSeekProgressChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress) {
+				flowLayout.setHorziontalPadding(progress);
+			}
+		});
+
+		radioGravity.setOnCheckedListener(new RadioLayout.OnCheckedListener() {
+			@Override
+			public void onChecked(View v, int position, boolean isChecked) {
+				if(isChecked) {
+					flowLayout.setGravity(position);
+				}
+			}
+		});
+
+
+		add.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final TextView tv = new TextView(getContext());
+				tv.setText(flowLayout.getChildCount() + "");
+				tv.setBackgroundColor(Color.RED);
+				tv.setLayoutParams(new ViewGroup.MarginLayoutParams(10 + new Random().nextInt(300), 68));
 				flowLayout.addView(tv);
 			}
-		}, 3000);
+		});
+
+		del.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				flowLayout.removeViewAt(flowLayout.getChildCount() - 1);
+			}
+		});
+
 		return view;
 	}
 
---
+++ improve/src/main/java/test/better/com/leak/custom_view/RadioLayout.java
@@ -0,0 +1,64 @@
+package test.better.com.leak.custom_view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+
+import test.better.com.leak.R;
+
+/**
+ * Created by cz on 16/3/7.
+ */
+public class RadioLayout extends RadioGroup {
+    private OnCheckedListener listener;
+
+    public RadioLayout(Context context) {
+        super(context);
+    }
+
+    public RadioLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RadioLayout);
+        setItems(a.getTextArray(R.styleable.RadioLayout_rl_items));
+        a.recycle();
+    }
+
+    /**
+     * 设置单选
+     *
+     * @param items
+     */
+    public void setItems(CharSequence[] items) {
+        if (null != items) {
+            removeAllViews();
+            int length = items.length;
+            for (int i = 0; i < length; i++) {
+                CharSequence item = items[i];
+                AppCompatRadioButton appCompatCheckBox = new AppCompatRadioButton(getContext());
+                appCompatCheckBox.setText(item);
+                final int index = i;
+                appCompatCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (null != listener && isChecked) {
+                            listener.onChecked(buttonView, index, isChecked);
+                        }
+                    }
+                });
+                addView(appCompatCheckBox);
+            }
+        }
+    }
+
+    public void setOnCheckedListener(OnCheckedListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnCheckedListener {
+        void onChecked(View v, int position, boolean isChecked);
+    }
+}
